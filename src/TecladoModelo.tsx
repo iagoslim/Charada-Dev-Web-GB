82ec2923cd15730f4b2f955488fd18f31cadf6c0
Alisson <49498497+AlissonRC@users.noreply.github.com>
2022-06-20 01:26:32 -0300

    Update TecladoModelo.tsx
    teclado style

--- src/TecladoModelo.tsx
+++ src/TecladoModelo.tsx
@@ -3,6 +3,7 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { minWidth } from "@mui/system";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -15,7 +16,7 @@ const Item = styled(Paper)(({ theme }) => ({
 const teclado = [
   ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
   ["A", "S", "D", "F", "G", "H", "J", "K", "L", "Ç"],
-  ["Enter", "Z", "X", "C", "V", "B", "N", "M", "Delete"],
+  ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE [X]"],
 ];
 
 export default function Teclado() {
@@ -25,7 +26,25 @@ export default function Teclado() {
         return (
           <div>
             {linha.map((tecla) => {
-              return <button>{tecla}</button>;
+              return (
+                <button
+                  style={{
+                    border: 0,
+                    backgroundColor: "#818384",
+                    borderRadius: "4px",
+                    height: "58px",
+                    minWidth: "43px",
+                    margin: "0 6px 10px 0",
+                    padding: "1px 6px",
+                    cursor: "pointer",
+                    color: "white",
+                    fontWeight: "bold",
+                    fontSize: "14px",
+                  }}
+                >
+                  {tecla}
+                </button>
+              );
             })}
           </div>
         );
