eec3a923766b888afdd366fc7e86178b1271d76d
AlissonRC <alissonrdacruz@gmail.com>
2022-06-24 14:07:57 -0300

    dale

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import BasicGrid from './Grid'
 import Teclado from './TecladoModelo'
+import { palavraRandom } from './listaDePalavras'
 
+let palavraCorreta = palavraRandom();
 
 function App() {
-  let isGameOver = false
-  let isGameWin = false
+  console.log(palavraCorreta)
+  const [isGameOver, setIsGameOver] = useState(false)
+  const [isGameWin, setIsGameWin] = useState(false)
   const [linha, setLinha] = useState(0)
   const [coluna, setColuna] = useState(0)
   const [tentativa, setTentativa] = useState([" ", " ", " ", " ", " "])
@@ -19,59 +22,99 @@ function App() {
     [" ", " ", " ", " ", " "],
   ])
 
+  
   function handleKeyPress(tecla: string){
     //const index = tentativa.findIndex((item)=> item===" ");
     
-    if(tecla === "DELETE [X]" && coluna > 0){
-      console.log(coluna);
-      setColuna(coluna-1);
-      console.log(coluna);
-      tentativa[coluna] = " ";
+    if(tecla === "DELETE [X]" && coluna > 0 && isGameOver === false){
+        tentativa[coluna-1] = " "
+        setTentativa([...tentativa])
+        console.log(tentativa)
+        console.log(coluna-1)
+        matriz[linha] = tentativa;
+        setMatriz([...matriz]);
+        setColuna(coluna-1)
+    }
+    else if (coluna<=4 && tecla.length === 1){
+      tentativa[coluna] = tecla;
       setTentativa([...tentativa]);
       matriz[linha] = tentativa;
       setMatriz([...matriz]);
-    }else{
-      if (coluna<=4){
-        tentativa[coluna] = tecla;
-        setTentativa([...tentativa]);
-        matriz[linha] = tentativa;
-        setMatriz([...matriz]);
-        console.log(coluna);
-        const temp = coluna+1;
-        setColuna(temp);
-        console.log(coluna);
-      }else if(tecla === "ENTER"){
+    
+      let temp = coluna;
+      temp++
+      setColuna(temp);
+      console.log(coluna)
+    }
+    
+      else if(tecla === "ENTER" && coluna === 5){
         if (validaTentativa(tentativa) === false){
           if(linha < 5){
             setLinha(linha+1);
             setTentativa([" ", " ", " ", " ", " "]);
             setColuna(0);
           }else{
+            setIsGameOver(true)
+            console.log(isGameOver)
             console.log("Fim de jogo malandro!");
           }
         }else{
+          setIsGameWin(true)
           console.log("Parabéns!");
         }
       }
-    }
+    
   }
-
+ 
   function validaTentativa(palavra: string[]){
-    if (palavra[0] === "C" &&
-        palavra[1] === "A" && 
-        palavra[2] === "R" &&
-        palavra[3] === "R" &&
-        palavra[4] === "O"
-    ){
+    let palavraString = ''
+    palavra.map((letra,index)=>{
+      console.log(letra)
+      palavraString = palavraString.concat(letra)
+    })
+    console.log(palavra)
+    console.log(palavraString)
+    if (palavraString === palavraCorreta.toUpperCase() ){
       return true;
     }else{
       return false;
     }
   }
-
+  function novoJogo(){
+    setIsGameOver(false)
+    setIsGameWin(false)
+    setMatriz([
+      [" ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " "],
+      [" ", " ", " ", " ", " "],
+    ]) 
+    setColuna(0)
+    setLinha(0)
+    setTentativa([" ", " ", " ", " ", " "])
+    palavraCorreta = palavraRandom()
+  }
   return (
     <div className="App">
+            {isGameOver && (
+            <div className='game-over'> 
+            Não foi dessa vez !
+            <p>A palavra era: {palavraCorreta}</p>
+            <button onClick={()=>novoJogo()}>Novo tentativa</button>
+            </div>
+          )}
+             {isGameWin && (
+            <div className='game-win'> 
+            Acertou !!!
+            <p>{palavraCorreta}</p>
+            <button onClick={()=>novoJogo()}>Novo Jogo</button>
+            </div>
+          )}
       <header className="App-header">
+
+
         <p>CHARADA</p>
         <BasicGrid palavra={matriz[0].join("")}/>
         <BasicGrid palavra={matriz[1].join("")}/>
